2537f3f54ec314f9ec3135756f21e6fff06b5b3e
yiqun12 <yix223@lehigh.edu>
2023-02-19 09:50:20 -0500

    fix the style in checkout page

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -55,88 +55,104 @@ function CardSection() {
           //console.log(snapshot.data())
         }
       });
-  
+
     return () => unsubscribe();
   }, []);
-  
+
   useEffect(() => {
     setTimeout(() => {
       document
-      .querySelector('#payment-method-form')
-      .addEventListener('submit', async (event) => {
-        event.preventDefault();
-        if (!event.target.reportValidity()) {
-          return;
-        }
-        console.log(customerData.current == null)
-        console.log(!elements)
-        console.log(!stripe)
-        document
-          .querySelectorAll('button')
-          .forEach((button) => (button.disabled = true));
-      //  console.log(customerData.current)
-        if (!stripe || !elements || customerData.current == null) {
+        .querySelector('#payment-method-form')
+        .addEventListener('submit', async (event) => {
+          event.preventDefault();
+          if (!event.target.reportValidity()) {
+            return;
+          }
+          console.log(customerData.current == null)
+          console.log(!elements)
+          console.log(!stripe)
           document
-          .querySelectorAll('button')
-          .forEach((button) => (button.disabled = false));
-          //document.querySelector('#error-message').textContent = "Too frequent operations";
-          // Stripe.js has not yet loaded.
-          // Make sure to disable form submission until Stripe.js has loaded.
-          return;
-        } else{
-  
-          // Payment method has not yet been added
-          // Proceed with adding payment method
-          const form = new FormData(event.target);
-          const cardholderName = form.get('name');
-  
-          console.log('user found in stripe');
-  
-          console.log(customerData.current);
-          const { setupIntent, error } = stripe.confirmCardSetup(
-            customerData.current.setup_secret,
-            {
-              payment_method: {
-                card: elements.getElement(CardElement),
-                billing_details: {
-                  name: cardholderName,
+            .querySelectorAll('button')
+            .forEach((button) => (button.disabled = true));
+          //  console.log(customerData.current)
+          if (!stripe || !elements || customerData.current == null) {
+            //document.querySelector('#prompt-message').textContent = "Too frequent operations";
+            // Stripe.js has not yet loaded.
+            // Make sure to disable form submission until Stripe.js has loaded.
+            document
+              .querySelectorAll('button')
+              .forEach((button) => (button.disabled = false));
+
+            return;
+          } else {
+
+            // Payment method has not yet been added
+            // Proceed with adding payment method
+            const form = new FormData(event.target);
+            const cardholderName = form.get('name');
+
+            console.log('user found in stripe');
+
+            console.log(customerData.current);
+            const { setupIntent, error } = stripe.confirmCardSetup(
+              customerData.current.setup_secret,
+              {
+                payment_method: {
+                  card: elements.getElement(CardElement),
+                  billing_details: {
+                    name: cardholderName,
+                  },
                 },
-              },
-            }
-          ).then(function (result) {
-           console.log(result.error);
-            if (result.error != null) {
-              console.log('error');
-              document.querySelector('#error-message').textContent = result.error.message;
-              document
-                .querySelectorAll('button')
-                .forEach((button) => (button.disabled = false));
-            } else if (result.setupIntent != null) {
-  
-              firebase
-                .firestore()
-                .collection('stripe_customers')
-                .doc(user.uid)
-                .collection('payment_methods')
-                .add({ id: result.setupIntent.payment_method })
-                .then(() => {
-                  // Payment method was successfully added
-                  console.log(result.setupIntent)
-                  document
+              }
+            ).then(function (result) {
+              console.log(result.error);
+              if (result.error != null) {
+                console.log('error');
+                document.querySelector('#prompt-message').textContent = result.error.message;
+                document
                   .querySelectorAll('button')
                   .forEach((button) => (button.disabled = false));
-                  document.querySelector('#error-message').textContent = "successfuly added!";
-                  saveId(Math.random())
-                  //customerData.current = null //cleanup
-                })
-  
-            }
-          });
-        }
-      });
+              } else if (result.setupIntent != null) {
+
+                firebase
+                  .firestore()
+                  .collection('stripe_customers')
+                  .doc(user.uid)
+                  .collection('payment_methods')
+                  .add({ id: result.setupIntent.payment_method })
+                  .then(() => {
+                    // Payment method was successfully added
+                    console.log(result.setupIntent)
+                    document
+                      .querySelectorAll('button')
+                      .forEach((button) => (button.disabled = false));
+                    // set the prompt message
+                    const promptMessage = document.querySelector('#prompt-message');
+                    promptMessage.textContent = "successfully added!";
+
+                    // hide the error message after 2 seconds
+                    setTimeout(() => {
+                      promptMessage.textContent = "";
+                    }, 6000);
+                    // reset the form inputs
+                    const form = document.querySelector('#payment-method-form');
+                    form.reset();
+                    // clear the card details input field
+                    if (elements) {
+                      const cardElement = elements.getElement(CardElement);
+                      cardElement.clear();
+                    }
+                    saveId(Math.random())
+                    //customerData.current = null //cleanup
+                  })
+
+              }
+            });
+          }
+        });
     }, 1);
-      
-  }, [customerData.current,stripe,elements]);
+
+  }, [customerData.current, stripe, elements]);
 
   //console.log(elements.getElement(CardElement))
   return (
@@ -146,33 +162,33 @@ function CardSection() {
 
           <div className="row-1">
             <div className="row row-2">
-              
+
               <span id="card2-inner">Card holder name</span>
             </div>
             <div className="row row-2">
               <input style={{
-'background-color': 'transparent',
-'border-color': 'transparent'
-              }}className = "card_input" type="text" name="name" required placeholder="Your name" />
+                'background-color': 'transparent',
+                'border-color': 'transparent'
+              }} className="card_input" type="text" name="name" required placeholder="Your name" />
             </div>
           </div>
 
-          <fieldset>
-            <div id="card-element">
-                
-              <div className="row-1" style={{ width: "100%" }}>
-            <div className="row row-2">
-              <span id="card2-inner">Card details</span>
-            </div>
-            <div className="row row-2">
-              <CardElement id="card-element" options={CARD_ELEMENT_OPTIONS} />
-            </div>
-          </div>
-          
+          <fieldset >
+            <div id="card-element" >
+
+            <div className="row-1" style={{ marginTop: 0, marginBottom: "14.432px", width: "100%" }}>
+                <div className="row row-2">
+                  <span id="card2-inner">Card details</span>
+                </div>
+                <div className="row row-2">
+                  <CardElement id="card-element" options={CARD_ELEMENT_OPTIONS} />
+                </div>
+              </div>
+
             </div>
           </fieldset>
-          <div id="error-message" role="alert"></div>
-          <button style={{width : "100%"}} class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save Card</button>
+          <div id="prompt-message" role="alert"></div>
+          <button style={{ width: "100%" }} class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save Card</button>
         </form>
       </div>
     </div>
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -8,6 +8,8 @@ import { useUserContext } from "../context/userContext";
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useMyHook } from '../pages/myHook';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function Checkout(props) {
   // Format amount for diplay in the UI
@@ -19,9 +21,19 @@ function Checkout(props) {
   useEffect(() => {
     //console.log('Component B - ID changed:', id);
   }, [id]);
+//ishvoer:
+
+const [isHover, setIsHover] = useState(false);
+
+const handleMouseEnter = () => {
+  setIsHover(true);
+};
+
+const handleMouseLeave = () => {
+  setIsHover(false);
+};
 
   function startDataListeners() {
-
     /**
      * Get all payment methods for the logged in customer
      */
@@ -32,7 +44,31 @@ function Checkout(props) {
       .collection('payment_methods')
       .onSnapshot((snapshot) => {
         if (snapshot.empty) {
-          document.querySelector('#add-new-card').open = true;
+          console.log('No payment methods found for the customer');
+        
+          //<option disabled="disabled" default="true"></option>
+          let optionElement = document.createElement('option');
+          optionElement.disabled = true;
+          optionElement.id = "404null"
+          optionElement.value = "null"
+          if(document.getElementById('404null')){
+          }else{
+            document.querySelector('select[name=payment-method]').appendChild(optionElement);
+            document.querySelector('#add-new-card').open = true;
+            document.querySelector('[name=delete]').setAttribute('disabled', true);
+            document.querySelector('[name=pay]').setAttribute('disabled', true);
+          }
+
+
+        } else {
+          // TODO: delete the empty button if card exists.
+          console.log('payment methods found for the customer');
+          if(document.getElementById('404null')){
+            const optionElementToDelete = document.querySelector(`option[id="${'404null'}"]`);
+            optionElementToDelete.remove();
+           }else{
+           }
+ 
         }
         snapshot.forEach(function (doc) {
           const paymentMethod = doc.data();
@@ -43,6 +79,7 @@ function Checkout(props) {
           const optionId = `card-${doc.id}`;
           let optionElement = document.getElementById(optionId);
 
+          console.log(document.getElementById(optionId))
           // Add a new option if one doesn't exist yet.
           if (!optionElement) {
             optionElement = document.createElement('option');
@@ -55,9 +92,11 @@ function Checkout(props) {
           optionElement.value = paymentMethod.id;
           optionElement.setAttribute("data-type", paymentMethod.card.brand);
           //console(optionElement.value)
-          optionElement.text = `•••• ${paymentMethod.card.last4} | ${paymentMethod.card.exp_month}/${paymentMethod.card.exp_year}`;
+          optionElement.text = `•••• ${paymentMethod.card.last4} | ${paymentMethod.card.exp_month}/${parseInt(paymentMethod.card.exp_year) % 100}`;
           optionElement.selected = true;
+
           saveId(Math.random())
+          // get the select element
           //console.log("exist card:",optionElement.text)
         });
       });
@@ -69,70 +108,76 @@ function Checkout(props) {
       .addEventListener('submit', async (event) => {
         event.preventDefault();
 
-          if (event.submitter.name === 'delete') {
-            document
+        if (event.submitter.name === 'delete') {
+          document
             .querySelectorAll('button')
             .forEach((button) => (button.disabled = true));
-            const form = new FormData(event.target);
-            const paymentMethodValue = form.get('payment-method');
-            const paymentMethodId = document.querySelector(`option[value="${paymentMethodValue}"]`).id;
-            const new_paymentMethodId = paymentMethodId.substring(5);
-            const user = JSON.parse(localStorage.getItem('user'));
-            console.log("deleted click")
-            console.log(new_paymentMethodId);
-            await firebase
-              .firestore()
-              .collection('stripe_customers')
-              .doc(user.uid)
-              .collection('payment_methods')
-              .doc(new_paymentMethodId)
-              .delete();
-              
-              const optionIdToDelete = paymentMethodId;
-              console.log(paymentMethodId)
-const optionElementToDelete = document.querySelector(`option[id="${optionIdToDelete}"]`);
-if (optionElementToDelete) {
-  optionElementToDelete.remove();
-}
-              document
-              .querySelectorAll('button')
-              .forEach((button) => (button.disabled = false));
-          }
-          if (event.submitter.name === 'pay') {
-            document
+          const form = new FormData(event.target);
+          const paymentMethodValue = form.get('payment-method');
+          const paymentMethodId = document.querySelector(`option[value="${paymentMethodValue}"]`).id;
+          const new_paymentMethodId = paymentMethodId.substring(5);
+          const user = JSON.parse(localStorage.getItem('user'));
+          console.log("deleted click")
+          console.log(new_paymentMethodId);
+          await firebase
+            .firestore()
+            .collection('stripe_customers')
+            .doc(user.uid)
+            .collection('payment_methods')
+            .doc(new_paymentMethodId)
+            .delete();
+            const optionIdToDelete = paymentMethodId;
+            const optionElementToDelete = document.querySelector(`option[id="${optionIdToDelete}"]`);
+            if (optionElementToDelete) {
+              optionElementToDelete.remove();
+            }
+          document
+            .querySelectorAll('button')
+            .forEach((button) => (button.disabled = false));
+                    // set the prompt message
+                    const promptMessage = document.querySelector('#delete-message');
+                    promptMessage.textContent = "successfully deleted!";
+
+                    // hide the error message after 2 seconds
+                    setTimeout(() => {
+                      promptMessage.textContent = "";
+                    }, 6000);
+        }
+        if (event.submitter.name === 'pay') {
+          document
             .querySelectorAll('button')
             .forEach((button) => (button.disabled = true));
-            const form = new FormData(event.target);
-            const amount = Number(totalPrice);
-            const currency = 'usd';
-            console.log(currency)
-            console.log(amount)
-            const dateTime = new Date().toISOString();
-            const date = dateTime.slice(0, 10) + '-' + dateTime.slice(11, 13) + '-' + dateTime.slice(14, 16) + '-' + dateTime.slice(17, 19) + '-' + dateTime.slice(20, 22);
-            console.log(form.get('payment-method'))
-            const user = JSON.parse(localStorage.getItem('user'));
-            const data = {
-              payment_method: form.get('payment-method'),
-              currency,
-              amount: amount,
-              status: 'new',
-              receipt: localStorage.getItem("products"),
-              dateTime: date,
-              user_email:user.email,
-            };
-            //console.log(data)
-    
-            await firebase
-              .firestore()
-              .collection('stripe_customers')
-              .doc(user.uid)
-              .collection('payments')
-              .add(data);
-    
-            document
-              .querySelectorAll('button')
-              .forEach((button) => (button.disabled = false));
-          }
+          const form = new FormData(event.target);
+          const amount = Number(totalPrice);
+          const currency = 'usd';
+          console.log(currency)
+          console.log(amount)
+          const dateTime = new Date().toISOString();
+          const date = dateTime.slice(0, 10) + '-' + dateTime.slice(11, 13) + '-' + dateTime.slice(14, 16) + '-' + dateTime.slice(17, 19) + '-' + dateTime.slice(20, 22);
+          console.log(form.get('payment-method'))
+          const user = JSON.parse(localStorage.getItem('user'));
+          const data = {
+            payment_method: form.get('payment-method'),
+            currency,
+            amount: amount,
+            status: 'new',
+            receipt: localStorage.getItem("products"),
+            dateTime: date,
+            user_email: user.email,
+          };
+          //console.log(data)
+
+          await firebase
+            .firestore()
+            .collection('stripe_customers')
+            .doc(user.uid)
+            .collection('payments')
+            .add(data);
+
+          document
+            .querySelectorAll('button')
+            .forEach((button) => (button.disabled = false));
+        }
 
       });
   }
@@ -177,10 +222,11 @@ if (optionElementToDelete) {
 
   return (
     <div>
-
+    <div id="card2-header">
+      <div id="add-new-card">
       <form id="payment-form">
         <div>
-          <label  style={{ width: '100%' }}>
+          <label style={{ width: '100%' }}>
 
             <div className="row row-1">
               <div className="col-2">
@@ -193,19 +239,27 @@ if (optionElementToDelete) {
               </div>
 
               <div className="col-7">
-              <select style={{ 'background-color': "#f5f7f9", color: "#9ca3af" }} name="payment-method" onChange={handleOptionChange} required>
-  <option  data-type="mastercard">Select Account</option>
-</select>
+                <select style={{ 'background-color': "#f5f7f9", color: "#9ca3af" }} name="payment-method" onChange={handleOptionChange} required>
+                  <option hidden data-type="mastercard">Select Account</option>
+                </select>
               </div>
               <div className="col-3 d-flex justify-content-center">
-              <button type="submit" style={{color: "#9ca3af"}}name="delete">Delete</button>
+                <button                 onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+             type="submit" style={{ 'color': isHover ? '#0a58ca' : '#444444' }}             
+
+            name="delete"><FontAwesomeIcon icon={faTrash} /></button>
+                
               </div>
             </div>
+            
           </label>
         </div>
-
-        <button type="submit" name="pay" style={{width : "100%"}}  class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pay ${totalPrice}</button>
+        <div id="delete-message" role="alert"></div>
+        <button type="submit" name="pay" style={{ width: "100%" }} class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pay ${totalPrice}</button>
       </form>
+    </div>
+    </div>
     </div>
   );
 };
