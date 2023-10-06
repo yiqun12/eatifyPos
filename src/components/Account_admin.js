d598af27e50536c5f1991353e9f690ea798ccead
Yutao-Li-306 <77427668+Yutao-Li-306@users.noreply.github.com>
2023-10-05 22:38:54 -0700

    changed side bar look and behavior

--- src/components/Account_admin.js
+++ src/components/Account_admin.js
@@ -506,6 +506,7 @@ const Account = () => {
       setActiveStoreTab(valueWithoutHash);
       setStoreName_(valueWithoutHash);
       setStoreID(valueWithoutHash);
+      setActiveStoreId(valueWithoutHash);
 
       // if there is a new store, use the default open time
       const default_Open_time = `{"0":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"},"1":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"},"2":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"},"3":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"},"4":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"},"5":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"},"6":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"},"7":{"timeRanges":[{"openTime":"xxxx","closeTime":"2359"}],"timezone":"ET"}}`;
@@ -547,6 +548,9 @@ const Account = () => {
 
 
 
+  // using the below to control if suboption popping and popping out depending on which store is selected on the side bar
+  const [activeStoreId, setActiveStoreId] = useState(null);
+
   return (
     <>
 
@@ -585,19 +589,135 @@ const Account = () => {
                 </div>
               </button>
               {storelist?.map((data, index) => (
-                <button
-                  className={`mt-2 btn mr-2 ${activeTab === `#${data.id}` ? 'border-black' : ''}`}
+                // <div>
+                // <button
+                //   className={`mt-2 btn mr-2 ${activeTab === `#${data.id}` ? 'border-black' : ''}`}
+                //   onClick={(e) => {
+                //     handleTabClick(e, `#${data.id}`);
+                //     setActiveStoreTab(data.id);
+                //     setShowSection('sales');
+                //     setStoreName_(data.Name);
+                //   }}
+                // >
+                //   <div style={{ alignItems: 'center', justifyContent: 'center' }}>
+                //     {data.Name}
+                //   </div>
+                // </button>
+                // </div>
+                <div
+                  // className={`mt-2 btn mr-2`}
                   onClick={(e) => {
                     handleTabClick(e, `#${data.id}`);
                     setActiveStoreTab(data.id);
                     setShowSection('sales');
                     setStoreName_(data.Name);
+                    setActiveStoreId(data.id)
                   }}
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column"
+                }}
                 >
-                  <div style={{ alignItems: 'center', justifyContent: 'center' }}>
+                  <div className={`mt-2 btn ${activeTab === `#${data.id}` ? 'border-black' : ''}`} style={{ alignItems: 'center', justifyContent: 'center', marginLeft: "2px", marginRight:"2px" }}>
                     {data.Name}
                   </div>
-                </button>
+
+                  {activeStoreId === data.id && (
+                  <ul className={`nav nav-tabs mt-4 overflow-x border-0 ${isMobile ? 'd-flex justify-content-between' : ''}`} style={{justifyContent:"inherit"}}>
+                    {/* <li className={`nav-item p-0`}
+                      onClick={(e) => {
+                        handleTabClick(e, '#profile');
+                        setShowSection('')
+                        window.location.hash = `person`;
+                      }}
+                    >
+                      <a className={`pt-0 nav-link ${activeTab === '#profile' || activeTab === '' ? 'active' : ''}`}>
+                        <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                          </svg>
+                        </i>
+                      </a>
+
+                    </li> */}
+
+                      <>
+                        <li className={`nav-item p-0`}
+                          onClick={() => {setShowSection('sales')
+                          window.location.hash = `charts`;}}
+                        >
+                          <a className={`pt-0 nav-link ${showSection === `sales` ? 'active' : ''}`}>
+                            <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
+                                <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z" />
+                              </svg>
+                            </i>
+                          </a>
+
+                        </li>
+                        <li className={`nav-item p-0`}
+                          onClick={() => {setShowSection('menu')
+                          window.location.hash = `book`;}}
+                        >
+                          <a className={`pt-0 nav-link ${showSection === `menu` ? 'active' : ''}`}>
+                            <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+                                <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+                              </svg>
+                            </i>
+                          </a>
+
+                        </li>
+
+                        <li className={`nav-item p-0`} onClick={() => {setShowSection('qrCode')
+                         window.location.hash = `code`;}}>
+                          <a className={`pt-0 nav-link ${showSection === `qrCode` ? 'active' : ''}`}>
+                            <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-qr-code" viewBox="0 0 16 16">
+                                <path d="M2 2h2v2H2V2Z" />
+                                <path d="M6 0v6H0V0h6ZM5 1H1v4h4V1ZM4 12H2v2h2v-2Z" />
+                                <path d="M6 10v6H0v-6h6Zm-5 1v4h4v-4H1Zm11-9h2v2h-2V2Z" />
+                                <path d="M10 0v6h6V0h-6Zm5 1v4h-4V1h4ZM8 1V0h1v2H8v2H7V1h1Zm0 5V4h1v2H8ZM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8H6Zm0 0v1H2V8H1v1H0V7h3v1h3Zm10 1h-1V7h1v2Zm-1 0h-1v2h2v-1h-1V9Zm-4 0h2v1h-1v1h-1V9Zm2 3v-1h-1v1h-1v1H9v1h3v-2h1Zm0 0h3v1h-2v1h-1v-2Zm-4-1v1h1v-2H7v1h2Z" />
+                                <path d="M7 12h1v3h4v1H7v-4Zm9 2v2h-3v-1h2v-1h1Z" />
+                              </svg>
+                            </i>
+                          </a>
+                        </li>
+                        <li className={`nav-item p-0`}
+                          onClick={() => { setShowSection('stripeCard')
+                          window.location.hash = `cards`;}}
+                        >
+                          <a className={`pt-0 nav-link ${showSection === `stripeCard` ? 'active' : ''}`}>
+                            <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
+                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z" />
+                                <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
+                              </svg>
+                            </i>
+                          </a>
+
+                        </li>
+                        <li className={`nav-item p-0`}
+                          onClick={() => {setShowSection('store')
+                          window.location.hash = `settings`;}}
+                        >
+                          <a className={`pt-0 nav-link ${showSection === `store` ? 'active' : ''}`}>
+                            <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                                <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                                <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                              </svg>
+                            </i>
+                          </a>
+
+                        </li>
+                      </>
+
+
+                  </ul>)
+}
+                </div>
               ))}
             </div>
           </nav>
@@ -633,7 +753,7 @@ const Account = () => {
                     </div>
                   </div>
 
-                  <div class="mt-2">
+                  <div class="mt-2 mb-2">
                     <Dropdown>
                       <Dropdown.Toggle
                         variant="neutral"
@@ -684,7 +804,7 @@ const Account = () => {
                   </div>
 
 
-                  <ul className={`nav nav-tabs mt-4 overflow-x border-0 ${isMobile ? 'd-flex justify-content-between' : ''}`}>
+                  {/* <ul className={`nav nav-tabs mt-4 overflow-x border-0 ${isMobile ? 'd-flex justify-content-between' : ''}`}>
                     <li className={`nav-item p-0`}
                       onClick={(e) => {
                         handleTabClick(e, '#profile');
@@ -780,7 +900,7 @@ const Account = () => {
 
 
 
-                  </ul>
+                  </ul> */}
                 </div>
               </div>
             </header>
