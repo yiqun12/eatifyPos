1b5034d23c496e143cf8043690075eda1a40ab6d
Yutao-Li-306 <77427668+Yutao-Li-306@users.noreply.github.com>
2023-10-06 21:05:36 -0700

    fixed css of sidebar in account_admin

--- src/components/Account_admin.js
+++ src/components/Account_admin.js
@@ -572,7 +572,7 @@ const Account = () => {
 
             <div className={`container-fluid `} style={{ "minHeight": "0px" }}>
             <button
-                className={`mt-2 btn mr-2 ${activeTab === '#profile' ? 'border-black' : ''}`}
+                className={`mt-2 btn mr-2 ml-2 ${activeTab === '#profile' ? 'border-black' : ''}`}
                 onClick={(e) => handleTabClick(e, '#profile')}
               >
                 <div style={{ alignItems: 'center', justifyContent: 'center' }}>
@@ -586,7 +586,7 @@ const Account = () => {
               </button>
               {storelist?.map((data, index) => (
                 <>                <div
-                className={`mt-2 btn mr-2 ${activeTab === `#${data.id}` ? 'border-black' : ''}`}
+                className={`mt-2 btn mr-2 ml-2 ${activeTab === `#${data.id}` ? 'border-black' : ''}`}
                 onClick={(e) => {
                   handleTabClick(e, `#${data.id}`);
                   setActiveStoreTab(data.id);
@@ -606,39 +606,40 @@ const Account = () => {
               {activeStoreId === data.id && (
                   <ul className={`nav nav-tabs mt-4 overflow-x border-0 flex flex-col`}>
                       <>
-                        <li className={`nav-item p-0` } style={{width:"80%"}}
+                        <li className={`nav-item p-0` } style={{width:"80%", margin: "auto", borderColor: "transparent !important"}}
                           onClick={() => {setShowSection('sales')
                           window.location.hash = `charts`;}}
                         >
-                          <a className={`pt-0 nav-link ${showSection === `sales` ? 'active' : ''}`}>
+                          <a className={`d-flex align-items-center pt-0 nav-link ${showSection === `sales` ? 'active' : ''}`} style={{marginLeft: "0", border: "0px"}}>
                             <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
                                 <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z" />
                               </svg>
                             </i>
-                            Daily Revenue
+                            <span style={{marginLeft: "5%"}}>Daily Revenue</span>
                           </a>
 
                         </li>
                         <li className={`nav-item p-0`}
                           onClick={() => {setShowSection('menu')
                           window.location.hash = `book`;}}
+                          style={{width:"80%", margin: "auto"}}
                         >
-                          <a className={`pt-0 nav-link ${showSection === `menu` ? 'active' : ''}`}>
+                          <a className={`d-flex align-items-center pt-0 nav-link ${showSection === `menu` ? 'active' : ''}`} style={{border: "0px"}}>
                             <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
                                 <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                               </svg>
                             </i>
-                            Menu Settings
+                            <span style={{marginLeft: "5%"}}>Menu Settings</span>
 
                           </a>
 
                         </li>
 
                         <li className={`nav-item p-0`} onClick={() => {setShowSection('qrCode')
-                         window.location.hash = `code`;}}>
-                          <a className={`pt-0 nav-link ${showSection === `qrCode` ? 'active' : ''}`}>
+                         window.location.hash = `code`}} style={{width:"80%", margin: "auto"}}>
+                          <a className={`d-flex align-items-center pt-0 nav-link ${showSection === `qrCode` ? 'active' : ''}`} style={{border: "0px"}}>
                             <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-qr-code" viewBox="0 0 16 16">
                                 <path d="M2 2h2v2H2V2Z" />
@@ -648,36 +649,39 @@ const Account = () => {
                                 <path d="M7 12h1v3h4v1H7v-4Zm9 2v2h-3v-1h2v-1h1Z" />
                               </svg>
                             </i>
-                            Table Order
+                            <span style={{marginLeft: "5%"}}>Table Order</span>
                           </a>
                         </li>
                         <li className={`nav-item p-0`}
                           onClick={() => { setShowSection('stripeCard')
                           window.location.hash = `cards`;}}
+                          style={{width:"80%", margin: "auto"}}
                         >
-                          <a className={`pt-0 nav-link ${showSection === `stripeCard` ? 'active' : ''}`}>
+                          <a className={`d-flex align-items-center pt-0 nav-link ${showSection === `stripeCard` ? 'active' : ''}`} style={{border: "0px"}}>
                             <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
                                 <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z" />
                                 <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
                               </svg>
                             </i>
-                            Notification
+                            <span style={{marginLeft: "5%"}}>Notification</span>
                           </a>
 
                         </li>
-                        <li className={`nav-item p-0`}
+                        <li className={`nav-item border-b-0 p-0`}
                           onClick={() => {setShowSection('store')
                           window.location.hash = data.id;}}
+                          style={{width:"80%", margin: "auto", border:"0px"}}
                         >
-                          <a className={`pt-0 nav-link ${showSection === `store` ? 'active' : ''}`}>
-                            <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                          <a className={`d-flex align-items-center pt-0 nav-link ${showSection === `store` ? 'active' : ''}`} style={{marginRight: "0", border: "0px"}}>
+                            <i className="scale-125 p-0 m-0" style={{ display: 'inline-block', verticalAlign: 'middle'}}>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
                                 <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
                                 <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
                               </svg>
                             </i>
-                            Store Settings
+
+                            <span style={{marginLeft: "5%"}}>Store Settings</span>
 
                           </a>
 
